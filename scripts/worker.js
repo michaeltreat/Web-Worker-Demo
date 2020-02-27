baf52a393a05bcbfe66193b4a15f3e7f32fe87b0
Michael Leon Treat <michael.leon.treat@gmail.com>
2020-02-27 15:10:08 -0800

    Update worker.js

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,9 +1,9 @@
 'use strict'
 
-let counter = 1000000001
+let counter = 1000000001;
 
-while( counter--){
-  if( counter % 100000000 === 0) console.log(`While loop is Currently at: ${counter}`) 
+while(counter--){
+  if(counter % 100000000 === 0) console.log(`While loop is Currently at: ${counter}`);
 }
 
-console.log('------------ While Loop has Ended! -----------')
+console.log('------------ While Loop has Ended! -----------');
